51c21f21bcb73b42a75d37f9b0394edd49ea997f
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-02-18 20:11:04 -0500

    inspect key

--- lib/actionkit_webhooks/airtable.ex
+++ lib/actionkit_webhooks/airtable.ex
@@ -2,7 +2,7 @@ defmodule ActionkitWebhooks.AirtableCache do
   use AirtableConfig
   import ShortMaps
 
-  def key, do: Application.get_env(:actionkit_webhooks, :airtable_key)
+  def key, do: Application.get_env(:actionkit_webhooks, :airtable_key) |> IO.inspect()
   def base, do: Application.get_env(:actionkit_webhooks, :airtable_base)
   def table, do: Application.get_env(:actionkit_webhooks, :airtable_table_name)
   def view, do: "Grid view"
